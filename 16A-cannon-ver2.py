0d38b4acb775df4fc7d93f7317bae1946352775c
TillliveOne <tillliveone@gmail.com>
2019-03-30 19:02:59 +0900

    공부한 내용 커밋
    기존에 공부한 내용으로 덮어 씌움

--- 16A-cannon-ver2.py
+++ 16A-cannon-ver2.py
@@ -20,30 +20,39 @@ def fire():
     if d < 25:
         t.color("blue")
         t.write("Good!",False,"center",("",15))
+        setnewtarget()
     else:
         t.color("red")
         t.write("Bad!",False,"center",("",15))
+        t.color("black")
+        t.goto(-200,10)
+        t.setheading(ang)
 
-    t.color("black")
+def init():
+    t.goto(-300,0)
+    t.down()
+    t.goto(300,0)
+    setnewtarget()
+
+def setstartposition():
+    t.up()
     t.goto(-200,10)
-    t.setheading(ang)
+    t.setheading(20)
 
-t.goto(-300,0)
-t.down()
-t.goto(300,0)
+def setnewtarget():
+    global target
+    target = random.randint(50,150)
+    t.pensize(3)
+    t.color("green")
+    t.up()
+    t.goto(target-25,2)
+    t.down()
+    t.goto(target+25,2)
+    t.color("black")
+    setstartposition()
 
-target = random.randint(50,150)
-t.pensize(3)
-t.color("green")
-t.up()
-t.goto(target-25,2)
-t.down()
-t.goto(target+25,2)
 
-t.color("black")
-t.up()
-t.goto(-200,10)
-t.setheading(20)
+init()
 
 t.onkeypress(turn_up,"Up")
 t.onkeypress(turn_down,"Down")
--- 18A-trun2.py
+++ 18A-trun2.py
@@ -1,87 +1,112 @@
-# 터틀런 만들기2
 import turtle as t
 import random
 
-score = 0           # 점수를 저장하는 변수
-playing = False     # 현재 게임 플레이 중인지 확인하는 변수
+score = 0
+playing = False
 
-te = t.Turtle()     # 악당 거북이(빨간색)
+te = t.Turtle()
 te.shape("turtle")
 te.color("red")
 te.speed(0)
 te.up()
-te.goto(0, 200)
+te.goto(0,200)
 
-ts = t.Turtle()     # 먹이(초록색 동그라미)
+ts = t.Turtle()
 ts.shape("circle")
 ts.color("green")
 ts.speed(0)
 ts.up()
 ts.goto(0, -200)
 
-def turn_right():                # 오른쪽으로 방향을 바꿉니다.
+def turn_right():
     t.setheading(0)
 
-def turn_up():                   # 위로 방향을 바꿉니다.
+def turn_up():
     t.setheading(90)
 
-def turn_left():                 # 왼쪽으로 방향을 바꿉니다.
+def turn_left():
     t.setheading(180)
 
-def turn_down():                 # 아래로 방향을 바꿉니다.
+def turn_down():
     t.setheading(270)
 
-def start():                    # 게임을 시작하는 함수
+def start():
     global playing
-    if playing == False:
+    if not playing:
         playing = True
-        t.clear()              # 메시지를 지웁니다
+        t.clear()
         play()
 
-def play():                    # 게임을 실제로 플레이하는 함수
+def play():
     global score
     global playing
-    t.forward(10)                       # 주인공 거북이 10만큼 앞으로 이동합니다.
-    if random.randint(1, 5) == 3:       # 1~5 사이에서 뽑은 수가 3이면(20% 확률)
+
+    # move of player
+    t.forward(10)
+    if t.xcor() > 250:
+        t.goto(-250,t.ycor())
+    if t.xcor() < -250:
+        t.goto(250, t.ycor())
+    if t.ycor() > 250:
+        t.goto(t.xcor(),-250)
+    if t.ycor() < -250:
+        t.goto(t.xcor(),250)
+
+
+    if random.randint(1,5) == 3:
         ang = te.towards(t.pos())
-        te.setheading(ang)              # 악당 거북이가 주인공 거북이를 바라보게 합니다.
-    speed = score + 5                   # 점수에 5를 더해서 속도를 올립니다(점수가 올라가면 빨라집니다).
-    if speed > 15:                      # 속도가 15를 넘지는 않도록 합니다.
+        te.setheading(ang)
+    speed = score + 5
+    if speed > 15:
         speed = 15
+
+    # move of enemy
     te.forward(speed)
-    if t.distance(te) < 12:             # 주인공과 악당의 거리가 12보다 작으면 게임을 종료합니다.
-        text = "Score : " + str(score)
-        message("Game Over", text)
+    if te.xcor() > 250:
+        te.goto(-250,te.ycor())
+    if te.xcor() < -250:
+        te.goto(250, te.ycor())
+    if te.ycor() > 250:
+        te.goto(te.xcor(),-250)
+    if te.ycor() < -250:
+        te.goto(te.xcor(),250)
+
+    # judge
+    if t.distance(te) < 12:
+        text = "Score:" + str(score)
+        message("Game Over",text)
         playing = False
         score = 0
-    if t.distance(ts) < 12:             # 주인공과 먹이의 거리가 12보다 작으면(가깝게 있으면)
-        score = score + 1               # 점수를 올립니다.
-        t.write(score)                  # 점수를 화면에 표시합니다.
-        star_x = random.randint(-230, 230)
-        star_y = random.randint(-230, 230)
-        ts.goto(star_x, star_y)         # 먹이를 다른 곳으로 옮깁니다.
+    if t.distance(ts) < 12:
+        score += 1
+        t.write(score)
+        star_x = random.randint(-230,230)
+        star_y = random.randint(-230,230)
+        ts.goto(star_x,star_y)
     if playing:
-        t.ontimer(play, 100)            # 게임 플레이 중이면 0.1초 후 play 함수를 실행합니다.
+        t.ontimer(play,100)
 
-def message(m1, m2):                    # 메시지를 화면에 표시하는 함수
+def message(m1,m2):
     t.clear()
-    t.goto(0, 100)
-    t.write(m1, False, "center", ("", 20))
-    t.goto(0, -100)
-    t.write(m2, False, "center", ("", 15))
+    t.goto(0,100)
+    t.write(m1,False,"center",("",20))
+    t.goto(0,-100)
+    t.write(m2,False,"center",("",15))
     t.home()
 
 t.title("Turtle Run")
-t.setup(500, 500)
+t.setup(500,500)
 t.bgcolor("orange")
-t.shape("turtle")   # 거북이 모양의 커서를 사용합니다.
-t.speed(0)          # 거북이 속도를 가장 빠르게로 지정합니다
+t.shape("turtle")
+t.speed(0)
 t.up()
 t.color("white")
-t.onkeypress(turn_right, "Right")   # [→]를 누르면 turn_right 함수를 실행하도록 합니다.
-t.onkeypress(turn_up, "Up")
-t.onkeypress(turn_left, "Left")
-t.onkeypress(turn_down, "Down")
-t.onkeypress(start, "space")
-t.listen()          # 거북이 그래픽 창이 키보드 입력을 받도록 합니다.
-message("Turtle Run", "[Space]")
+t.onkeypress(turn_right,"Right")
+t.onkeypress(turn_up,"Up")
+t.onkeypress(turn_left,"Left")
+t.onkeypress(turn_down,"Down")
+t.onkeypress(start,"space")
+t.listen()
+message("Turtle Run", "[Press the space key]")
+t.mainloop()
+
